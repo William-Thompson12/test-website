82c47778675f86d3af3041ea59b042fb4816c649
Michael Kinful <epyon85@gmail.com>
2020-09-22 21:26:44 -0400

    log in

---
+++ main.js
@@ -0,0 +1,9 @@
+let log = document.getElementById("login");
+let reg = document.getElementById("register");
+let button = document.getElementById("btn");
+
+function login(){
+    log.style.left = "50px";
+    reg.style.left = "450px";
+    button.style.left = "0px";
+}
